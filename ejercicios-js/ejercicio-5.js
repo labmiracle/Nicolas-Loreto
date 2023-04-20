923897d7c7b81e3abd0a11e068ed057163b5f28a
NicoLoreto <nicoloreto2@gmail.com>
2023-04-20 14:16:34 -0300

    ejercicio-5 added

---
+++ ejercicios-js/ejercicio-5.js
@@ -0,0 +1,27 @@
+function camelize(str) {
+
+    if(typeof str != 'string'){
+
+        return 'No es una cadena de caracteres'
+
+    };
+
+    let arr = str.split(' ');
+
+    let aMayuscula = [];
+
+    for (palabra of arr){
+
+        aMayuscula.push(palabra.charAt(0).toUpperCase() + palabra.slice(1))
+
+    };
+
+    let string = aMayuscula.join('');
+
+    let camelCase = string.charAt(0).toLowerCase() + string.slice(1);
+
+    return camelCase;
+
+}
+
+console.assert(camelize('Hola a todos que tal') === 'holaATodosQueTal');

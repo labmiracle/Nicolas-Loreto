7f34b67a11aa90757ac02e72d710fd167e627055
NicoLoreto <nicoloreto2@gmail.com>
2023-04-24 21:18:48 -0300

    recursive function added

--- ejercicios-js/ejercicio-2.js
+++ ejercicios-js/ejercicio-2.js
@@ -1,18 +1,18 @@
 function sum (arr, n) {
 
-    let suma  = 0;
+  if(n === 0){
+    return 0;
+  }else if(n < 0){
+    return 'n no puede ser un número negativo';
+  }else{
+    return sum(arr, n-1) + arr[n-1];
+  };
 
-    for( let i = 0; i <= n - 1; i++){
+ };
 
-        if (n > arr.length){
-            return 'El parámetro n supera la longitud del parámetro arr'
-        }
-        else{
-            suma += arr[i] 
-        }      
-    }
-
-    return suma
-
- }
- sum([1,3,102,7,10], 3)
+console.assert (sum([1,3,1,7,10], 3) === 5);
+console.assert (sum([3,7,0,7,10], 2) === 10);
+console.assert (sum([6,2,4,7,10], 5) === 29);
+console.assert (sum([9,2,90,7,10], 4) === 108);
+ 
+ 

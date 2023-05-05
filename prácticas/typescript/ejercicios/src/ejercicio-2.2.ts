033df7fb2021308b98d2ab634af030097107048b
NicoLoreto <nicoloreto2@gmail.com>
2023-05-05 18:15:58 -0300

    ejercicio-2.2 added

---
+++ prácticas/typescript/ejercicios/src/ejercicio-2.2.ts
@@ -0,0 +1,19 @@
+function padLeft(value: string, padding: number | string): string {
+
+    if( typeof padding === 'number' && padding >= 0){
+        return `${Array(padding + 1).join(' ')}${value}`
+    }
+    else if(typeof padding === 'string'){
+        return padding + value
+    }
+return 'los valores no son validos'
+    }
+
+console.log('[Ejercicio 4.2]', `
+${padLeft('', 7)}
+${padLeft('', '')}
+${padLeft('', '')}
+${padLeft('', '')}
+${padLeft('', '')}
+`);
+    

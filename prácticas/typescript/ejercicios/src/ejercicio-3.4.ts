2a1a7d22197c28569810f724e1502fe5327a8ea8
NicoLoreto <nicoloreto2@gmail.com>
2023-05-07 04:31:14 -0300

    ejercicio-3.4 added

---
+++ prácticas/typescript/ejercicios/src/ejercicio-3.4.ts
@@ -0,0 +1,10 @@
+function greet(greeting?: string): string {
+    if(typeof greeting === 'undefined'){
+        return 'hola'
+    }
+    return greeting.toUpperCase();
+}
+const defaultGreeting = greet();
+const portugueseGreeting = greet('Oi como vai!');
+console.log('[Ejercicio 3.4]', defaultGreeting, portugueseGreeting);
+

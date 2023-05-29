81ab75b122a87b4fb43dc62e02abda9807116976
NicoLoreto <nicoloreto2@gmail.com>
2023-05-28 22:18:15 -0300

    ejercicio-1.5 updated

--- prácticas/jest/dist/ejercicio-1.5.js
+++ prácticas/jest/dist/ejercicio-1.5.js
@@ -1,15 +1,10 @@
-export function findRepeat(string: string): number {
-
+export function findRepeat (string) {
     const lowerCase = string.toLowerCase();
-
     const arr = lowerCase.split('');
-
     for (let i = 0; i < arr.length; i++) {
-
         if (arr[i] === arr[i + 1] && arr[i] != ' ') {
             return i;
         }
-
     }
     return -1;
 }

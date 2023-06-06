27831b82eaf641db1157452203104f38c0adf62b
NicoLoreto <nicoloreto2@gmail.com>
2023-06-06 18:51:03 -0300

    ejercicio-map-3 added

--- prácticas/programación-funcional/src/dist/map.js
+++ prácticas/programación-funcional/src/dist/map.js
@@ -1,40 +1,51 @@
 "use strict";
 const characters = [
     {
-        name: 'Luke Skywalker',
-        height: '172',
-        mass: '77',
-        eye_color: 'blue',
-        gender: 'male',
+        name: "Luke Skywalker",
+        height: "172",
+        mass: "77",
+        eye_color: "blue",
+        gender: "male",
     },
     {
-        name: 'Darth Vader',
-        height: '202',
-        mass: '136',
-        eye_color: 'yellow',
-        gender: 'male',
+        name: "Darth Vader",
+        height: "202",
+        mass: "136",
+        eye_color: "yellow",
+        gender: "male",
     },
     {
-        name: 'Leia Organa',
-        height: '150',
-        mass: '49',
-        eye_color: 'brown',
-        gender: 'female',
+        name: "Leia Organa",
+        height: "150",
+        mass: "49",
+        eye_color: "brown",
+        gender: "female",
     },
     {
-        name: 'Anakin Skywalker',
-        height: '188',
-        mass: '84',
-        eye_color: 'blue',
-        gender: 'male',
-    }
+        name: "Anakin Skywalker",
+        height: "188",
+        mass: "84",
+        eye_color: "blue",
+        gender: "male",
+    },
 ];
 function getName(arr) {
-    const map = arr.map(n => n.name);
+    const map = arr.map((n) => n.name);
     return map;
 }
 function getHeight(arr) {
-    const map = arr.map(h => h.height);
+    const map = arr.map((h) => h.height);
+    return map;
+}
+function getNameAndHeight(arr) {
+    const map = arr.map((o) => {
+        return {
+            name: o.name,
+            height: o.height,
+        };
+    });
     return map;
 }
 getName(characters);
+getHeight(characters);
+getNameAndHeight(characters);
--- prácticas/programación-funcional/src/map.ts
+++ prácticas/programación-funcional/src/map.ts
@@ -1,56 +1,64 @@
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: '172',
-        mass: '77',
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: '202',
-        mass: '136',
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: '150',
-        mass: '49',
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: '188',
-        mass: '84',
-        eye_color: 'blue',
-        gender: 'male',
-    }
-    ];
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
 
-    interface Person{
-        name: string,
-        height: string,
-        mass: string,
-        eye_color: string,
-        gender: string,
-    }
+interface Person {
+  name: string;
+  height: string;
+  mass: string;
+  eye_color: string;
+  gender: string;
+}
 
-    
-    function getName(arr: Person[]): {}{
-    
-        const map = arr.map(n => n.name)
-    
-        return map
-    }
+function getName(arr: Person[]): {} {
+  const map = arr.map((n) => n.name);
 
-    function getHeight(arr: Person[]){
+  return map;
+}
 
-        const map = arr.map(h => h.height)
-        return map
+function getHeight(arr: Person[]) {
+  const map = arr.map((h) => h.height);
+  return map;
+}
 
-    }
-    
+function getNameAndHeight(arr: Person[]) {
+  const map = arr.map((o) => {
+    return {
+      name: o.name,
+      height: o.height,
+    };
+  });
 
-    getName(characters)
+  return map;
+}
+
+getName(characters);
+getHeight(characters);
+getNameAndHeight(characters);

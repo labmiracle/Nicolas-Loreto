8d9cb5ce516afcf781583344a8773d8b62c274a8
NicoLoreto <nicoloreto.dev@gmail.com>
2023-06-28 19:16:49 -0300

    else if deleted

--- prácticas/javascript/ejercicio-1.js
+++ prácticas/javascript/ejercicio-1.js
@@ -13,28 +13,28 @@ function golfScore (par, strokes) {
    if (strokes <= 0 || par <= 0) {
       return 'Los valores no son correctos';
    }
-   else if (strokes === 1) {
+   if (strokes === 1) {
       return names[0];
    }
-   else if (strokes <= par - 2) {
+   if (strokes <= par - 2) {
       return names[1];
    }
-   else if (strokes === par - 1) {
+   if (strokes === par - 1) {
       return names[2];
    }
-   else if (strokes === par) {
+   if (strokes === par) {
       return names[3];
    }
-   else if (strokes === par + 1) {
+   if (strokes === par + 1) {
       return names[4];
    }
-   else if (strokes === par + 2) {
+   if (strokes === par + 2) {
       return names[5];
    }
-   else if (strokes >= par + 3) {
+   if (strokes >= par + 3) {
       return names[6];
    }
 }
 
-golfScore(5, 4)
+console.log(golfScore(5, 1))
 

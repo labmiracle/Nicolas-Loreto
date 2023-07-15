22d30360a9fc4de78d61bb15c91dae3478cc2945
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-14 22:42:57 -0300

    exercise-1 added

--- prácticas/estructuras-de-datos/src/Listas-enlazadas/exercise-1.js
+++ prácticas/estructuras-de-datos/src/Listas-enlazadas/exercise-1.js
@@ -1,20 +1,25 @@
-// class Node with data and reference
+// class Node with data and next reference
 class Node {
-  constructor(data, ref = null) {
+  constructor(data, next = null) {
     this.data = data;
-    this.ref = ref;
+    this.next = next;
   }
 }
 
-// class LinkedList with head, size and insert method
+// class LinkedList
 class LinkedList {
   constructor() {
     this.head = null;
     this.size = 0;
   }
-  insert(data) {
+
+  // size method
+  sizeCurrent() {
+    console.log(`the current size of the LinkedList is ${this.size}`);
+  }
+  add(value) {
     // create "node" object
-    let node = new Node(data);
+    let node = new Node(value);
     // create current node
     let current;
     // if this.head is null, node becomes this.head
@@ -24,19 +29,87 @@ class LinkedList {
       // if this.head is not null, current becomes this.head
       current = this.head;
       // searching for the null value using while
-      while (current.ref) {
-        current = current.ref;
+      while (current.next) {
+        current = current.next;
       }
-      // insert value
-      current.ref = node;
+      // add value
+      current.next = node;
     }
     // increment size
     this.size++;
+    console.log(`'${value}' has been added`);
+  }
+
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  remove(value) {
+    // start by this.head
+    let current = this.head;
+
+    // if LinkedList is empty return
+    if (!current) return;
+
+    let previus;
+
+    // advance for the list
+    while (current && current.data != value) {
+      previus = current;
+      current = current.next;
+    }
+
+    // value not found
+
+    if (!current) {
+      console.log(`'${value}' is not found for remove`);
+      return;
+    }
+
+    // if previous is null then the first element is the value and this element is deleted
+    // else the current is deleted
+
+    if (!previus) {
+      this.head = current.next;
+    } else {
+      previus.next = current.next;
+    }
+    this.size--;
+    console.log(`'${value}' has been removed`);
+  }
+
+  find(value) {
+    let current;
+    current = this.head;
+
+    while (current && current.data != value) {
+      current = current.next;
+    }
+
+    if (!current) {
+      console.log(`'${value}' is not found`);
+    } else {
+      console.log(`'${value}' found`);
+    }
   }
 }
 
 let lista = new LinkedList();
 
-lista.insert('hello world');
-lista.insert('Head 2');
+lista.sizeCurrent();
+lista.add('hello world');
+lista.find('hello world');
+lista.add('this is a new node');
+lista.sizeCurrent();
+lista.remove('hello');
+lista.add('and another one');
+lista.sizeCurrent();
+lista.find('and another one');
+lista.find('hello world');
+lista.remove('hello world');
+lista.sizeCurrent();
+lista.remove('this is a new node');
+lista.sizeCurrent();
+
 console.log(lista);

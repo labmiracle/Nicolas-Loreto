f9eb79aae7351f2237a4822f5048dc48453ace07
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-28 16:22:44 -0300

    Tree added(WIP)

---
+++ prácticas/estructura-de-datos/src/Trees/Tree.ts
@@ -0,0 +1,76 @@
+export interface INode {
+  data: number | null;
+  left: INode | null;
+  right: INode | null;
+}
+
+export class Node implements INode {
+  constructor(data: number | null, left: INode | null = null, right: INode | null = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+  data: number | null;
+  left: INode | null;
+  right: INode | null;
+}
+
+export class Tree {
+  root: INode | null;
+  size: number;
+  constructor() {
+    this.root = null;
+    this.size = 0;
+  }
+
+  // method for add a new value
+  add(value: number) {
+    const node = new Node(value);
+
+    if (!this.root) {
+      this.root = node;
+    } else {
+      this.addNode(this.root, node);
+    }
+
+    this.size++;
+    console.log(`'${value}' has been added`);
+  }
+
+  // add in order
+  addNode(node: INode, newNode: INode) {
+    // if the new node is less than the current node the new node is added to the left
+    if (newNode.data! < node.data!) {
+      if (!node.left) {
+        node.left = newNode;
+      } else {
+        this.addNode(node.left, newNode);
+      }
+    }
+
+    // if the new node is greater than the current node the new node is added to the right
+    if (newNode.data! > node.data!) {
+      if (!node.right) {
+        node.right = newNode;
+      } else {
+        this.addNode(node.right, newNode);
+      }
+    }
+  }
+
+  // mehod for count nodes
+  sizeCurrent() {
+    return this.size;
+  }
+}
+
+let n = new Tree();
+n.add(60);
+n.add(80);
+n.add(20);
+n.add(30);
+n.add(10);
+n.add(50);
+n.add(90);
+console.log(n.sizeCurrent());
+console.log(n);

1cd762d5b09f8f343ee9597fb92d157a4f30320b
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-29 20:15:45 -0300

    sending string from server

--- prácticas/node/src/app.js
+++ prácticas/node/src/app.js
@@ -3,6 +3,10 @@ const http = require('http');
 const port = 3000;
 const ip = 'localhost';
 
-http.createServer((req, res) => {}).listen(port, ip);
+http
+  .createServer((req, res) => {
+    res.end('Hi from the server!');
+  })
+  .listen(port, ip);
 
 console.log('server started on ', port);

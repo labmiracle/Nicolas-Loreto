265f7939fd983ef1343cf786410703b706e78a26
NicoLoreto <nicoloreto.dev@gmail.com>
2023-07-29 20:38:36 -0300

    process.env added

--- prácticas/node/src/app.js
+++ prácticas/node/src/app.js
@@ -1,10 +1,11 @@
 const http = require('http');
 
-const port = 3000;
-const ip = 'localhost';
+let port = process.env.PORT || 3000;
+const ip = process.env.IP || 'localhost';
 
 http
   .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hi from the server!');
   })
   .listen(port, ip);

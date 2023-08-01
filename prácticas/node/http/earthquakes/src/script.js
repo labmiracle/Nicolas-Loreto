a0fc7d99d0a95a23331e437ebb232d9a6982ce23
NicoLoreto <nicoloreto.dev@gmail.com>
2023-08-01 14:30:17 -0300

    object with data created (WIP)

--- prácticas/node/http/earthquakes/src/script.js
+++ prácticas/node/http/earthquakes/src/script.js
@@ -4,17 +4,25 @@ const request = async (magnitud) => {
     const response = await fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
     const data = await response.json();
 
-    const object = {
-      title: data.metadata.title,
-      total: data.metadata.count,
-      status: data.metadata.status,
-    };
-    console.log(object);
     const arr = data.features;
 
     arr.map((m) => {
       const mag = m.properties.mag;
       if (mag >= magnitud) {
+        const object = {
+          title: data.metadata.title,
+          total: data.metadata.count,
+          status: data.metadata.status,
+          title_earthquakes: m.properties.title,
+          date: '',
+          time: '',
+          mag: mag,
+          status_earthquakes: m.properties.status,
+          type: m.properties.type,
+          place_earthquakes: m.properties.place,
+          coordinate: m.geometry.coordinates[0],
+        };
+        console.log(object);
       }
     });
   } catch (error) {

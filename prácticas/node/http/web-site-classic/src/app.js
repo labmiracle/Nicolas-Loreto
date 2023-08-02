d6ae6d1d18451c5b5e3ba635cefcf0a6c848a3fc
NicoLoreto <nicoloreto.dev@gmail.com>
2023-08-02 20:08:45 -0300

    formatted template

--- prácticas/node/http/web-site-classic/src/app.js
+++ prácticas/node/http/web-site-classic/src/app.js
@@ -14,137 +14,137 @@ http
       res.writeHead(200, { 'Content-type': 'text/html' });
 
       let html = `   
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Web site classic</title>
-  </head>
-  <body>
-  <h1>Welcome to the website!</h1>
-  <h2>Available urls:</h2>
-  <ul>
-    <li>
-        <a href='http://${url}/about-us'>
-        ${url}/about-us
-        </a>
-      </li>
+                  <!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Web site classic</title>
+                  </head>
+                  <body>
+                  <h1>Welcome to the website!</h1>
+                  <h2>Available urls:</h2>
+                  <ul>
+                    <li>
+                        <a href='http://${url}/about-us'>
+                        ${url}/about-us
+                        </a>
+                      </li>
 
-  <li>
-       <a href='http://${url}/where-we-are'>
-        ${url}/where-we-are
-      </a>
-    </li>
-    <li>
-      <a href='http://${url}/what-do-we-do'>
-      ${url}/what-do-we-do
-    </a>
-  </li>
-  <li>
-      <a href='http://${url}/contact'>
-      ${url}/contact
-  </a>
-</li>
-</ul>  
-  </body>
-  </html>`;
+                  <li>
+                      <a href='http://${url}/where-we-are'>
+                        ${url}/where-we-are
+                      </a>
+                    </li>
+                    <li>
+                      <a href='http://${url}/what-do-we-do'>
+                      ${url}/what-do-we-do
+                    </a>
+                  </li>
+                  <li>
+                      <a href='http://${url}/contact'>
+                      ${url}/contact
+                  </a>
+                </li>
+                </ul>  
+                  </body>
+                  </html>`;
 
       res.end(html);
     } else if (req.url === '/about-us' && req.method === 'GET') {
       res.writeHead(200, { 'Content-type': 'html' });
 
       let html = `   
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Web site classic</title>
-  </head>
-  <body>
-  <h1>${req.url}</h1>
-  <h2>About us</h2>
-  
-  </body>
-  </html>`;
+                  <!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Web site classic</title>
+                  </head>
+                  <body>
+                  <h1>${req.url}</h1>
+                  <h2>About us</h2>
+                  
+                  </body>
+                  </html>`;
 
       res.end(html);
     } else if (req.url === '/where-we-are' && req.method === 'GET') {
       res.writeHead(200, { 'Content-type': 'text/html' });
 
       let html = `   
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Web site classic</title>
-  </head>
-  <body>
-  <h1>${req.url}</h1>
-  <h2>Where we are</h2>
-  
-  </body>
-  </html>`;
+                  <!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Web site classic</title>
+                  </head>
+                  <body>
+                  <h1>${req.url}</h1>
+                  <h2>Where we are</h2>
+                  
+                  </body>
+                  </html>`;
 
       res.end(html);
     } else if (req.url === '/what-do-we-do' && req.method === 'GET') {
       res.writeHead(200, { 'Content-type': 'text/html' });
 
       let html = `   
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Web site classic</title>
-  </head>
-  <body>
-  <h1>${req.url}</h1>
-  <h2>What do we do</h2>
-  
-  </body>
-  </html>`;
+                  <!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Web site classic</title>
+                  </head>
+                  <body>
+                  <h1>${req.url}</h1>
+                  <h2>What do we do</h2>
+                  
+                  </body>
+                  </html>`;
 
       res.end(html);
     } else if (req.url === '/contact' && req.method === 'GET') {
       res.writeHead(200, { 'Content-type': 'text/html' });
 
       let html = `   
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Web site classic</title>
-  </head>
-  <body>
-  <h1>${req.url}</h1>
-  <h2>Contact</h2>
-  
-  </body>
-  </html>`;
+                  <!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Web site classic</title>
+                  </head>
+                  <body>
+                  <h1>${req.url}</h1>
+                  <h2>Contact</h2>
+                  
+                  </body>
+                  </html>`;
 
       res.end(html);
     } else {
       res.writeHead(404, { 'Content-type': 'text/html' });
 
       let html = `   
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Web site classic</title>
-  </head>
-  <body>
-  <h1>${req.url}</h1>
-  <h2>Error page</h2>
-  <p>File not found</p>
-  
-  </body>
-  </html>`;
+                  <!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Web site classic</title>
+                  </head>
+                  <body>
+                  <h1>${req.url}</h1>
+                  <h2>Error page</h2>
+                  <p>File not found</p>
+                  
+                  </body>
+                  </html>`;
 
       res.end(html);
     }
--- prácticas/node/http/web-site-miracle-lab/src/app.js
+++ prácticas/node/http/web-site-miracle-lab/src/app.js
@@ -18,88 +18,89 @@ http
 
       if (req.url === '/miracle') {
         let html = `
-    
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Miracle</title>
-      </head>
-      <body>
-      <span>The current url is: ${req.url}<span>  
-      <h1>Welcome to Miracle!</h1>
-      <h3>This is a web site of Miracle</h3>     
-      </body>
-      </html>`;
+                  
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Miracle</title>
+                    </head>
+                    <body>
+                    <span>The current url is: ${req.url}<span>  
+                    <h1>Welcome to Miracle!</h1>
+                    <h3>This is a web site of Miracle</h3>     
+                    </body>
+                    </html>`;
 
         res.end(html);
       } else if (req.url === '/about') {
         let html = `
     
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Miracle</title>
-      </head>
-      <body>
-      <span>The current url is: ${req.url}<span>  
-      <h2>About us</h2>
-      <p>With over 10 years of experience working in industries as diverse as Insurance, Retail, Oil & Gas and Aerospace, we have become subject matter experts in our domains, achieving a high degree of expertise that shows in the quality of the solutions we deliver for our clients.<p>     
-      </body>
-      </html>`;
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Miracle</title>
+                    </head>
+                    <body>
+                    <span>The current url is: ${req.url}<span>  
+                    <h2>About us</h2>
+                    <p>With over 10 years of experience working in industries as diverse as Insurance, Retail, Oil & Gas and Aerospace, we have become subject matter experts in our domains, achieving a high degree of expertise that shows in the quality of the solutions we deliver for our clients.<p>     
+                    </body>
+                    </html>`;
 
         res.end(html);
       } else if (req.url === '/') {
         let html = `
     
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Miracle</title>
-      </head>
-      <body>
-      <span>The current url is empty<span>  
-      <h2>Available urls:</h2>
-      <ul>
-        <li>
-            <a href='${server}/miracle'>
-              ${server}/miracle
-            </a>
-          </li>
-  
-      <li>
-          <a href='${server}/about'>
-              ${server}/about
-          </a>
-        </li>
-    </ul>  
-      </body>
-      </html>`;
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Miracle</title>
+                    </head>
+                    <body>
+                    <span>The current url is empty<span>  
+                    <h2>Available urls:</h2>
+                    <ul>
+                      <li>
+                          <a href='${server}/miracle'>
+                            ${server}/miracle
+                          </a>
+                        </li>
+                
+                    <li>
+                        <a href='${server}/about'>
+                            ${server}/about
+                        </a>
+                      </li>
+                  </ul>  
+                    </body>
+                    </html>`;
 
         res.end(html);
       } else {
         res.writeHead(404, { 'Content-type': 'html' });
         let html = `
     
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Miracle</title>
-      </head>
-      <body>
-      <span>The current url is: ${req.url}<span>  
-      <h2>Error page</h2>
-      <h3>404</h3>
-      <h4>File not found</h4>
-      </body>
-      </html>`;
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Miracle</title>
+                    </head>
+                    <body>
+                    <span>The current url is: ${req.url}<span>  
+                    <h2>Error page</h2>
+                    <h3>404</h3>
+                    <h4>File not found</h4>
+                    </body>
+                    </html>`;
+
         res.end(html);
       }
     } catch (error) {

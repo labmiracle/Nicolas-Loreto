32e19c85659ef9e1725b21b16d3a01e89e7c8470
NicoLoreto <nicoloreto.dev@gmail.com>
2023-08-02 20:11:09 -0300

    about template served

--- prácticas/node/fs/src/server.js
+++ prácticas/node/fs/src/server.js
@@ -6,23 +6,41 @@ const ip = process.env.IP | 'localhost';
 
 http
   .createServer((req, res) => {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+    try {
+      if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
 
-      console.log(__dirname);
+        // display the current value of the directory
+        res.end(template);
+      }
+    } catch (error) {
+      throw error;
     }
   })
   .listen(port, ip);
 
-// current directoy
+// current directory
 const currentDirectory = () => {
   console.log('the current directory is: ', __dirname);
+  return __dirname;
 };
 
 // read file with fs module
-fs.readFile('text.txt', 'utf-8', (error, data) => {
+fs.readFileSync('text.txt', 'utf-8', (error, data) => {
   if (!error) {
     console.log(data);
+    return data;
+  } else {
+    throw error;
+  }
+});
+
+// read the html file
+
+const template = fs.readFileSync('vistas/about.html', 'utf-8', (error, data) => {
+  if (!error) {
+    console.log(data);
+    return data;
   } else {
     throw error;
   }

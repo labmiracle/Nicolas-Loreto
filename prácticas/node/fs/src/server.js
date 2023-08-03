1c8c39c9dd264f00b46d446cf038c244c67a6216
NicoLoreto <nicoloreto.dev@gmail.com>
2023-08-02 22:27:52 -0300

    404 template added

--- prácticas/node/fs/src/server.js
+++ prácticas/node/fs/src/server.js
@@ -1,20 +1,29 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const port = process.env.PORT | 5000;
 const ip = process.env.IP | 'localhost';
 
 http
   .createServer((req, res) => {
-    try {
-      if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+    const url = req.url;
+    let filePath;
 
-        // display the current value of the directory
-        res.end(template);
-      }
-    } catch (error) {
-      throw error;
+    if (url != '/') {
+      filePath = path.join(currentDirectory(), 'vistas/404.html');
+      fs.readFile(filePath, (err, data) => {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+      });
+    } else {
+      filePath = path.join(currentDirectory(), 'vistas/about.html');
+      fs.readFile(filePath, (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+      });
     }
   })
   .listen(port, ip);
@@ -23,6 +32,11 @@ http
 const currentDirectory = () => {
   console.log('the current directory is: ', __dirname);
   return __dirname;
+};
+
+//file
+const file = () => {
+  return __filename;
 };
 
 // read file with fs module
@@ -35,16 +49,5 @@ fs.readFileSync('text.txt', 'utf-8', (error, data) => {
   }
 });
 
-// read the html file
-
-const template = fs.readFileSync('vistas/about.html', 'utf-8', (error, data) => {
-  if (!error) {
-    console.log(data);
-    return data;
-  } else {
-    throw error;
-  }
-});
-
 currentDirectory();
-console.log('server started on', port);
+console.log('server started on port', port);
